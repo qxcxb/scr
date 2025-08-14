c8164932cbfececa343822e635724fe7e5bdacbd
qxcxb <155810624+qxcxb@users.noreply.github.com>
2025-08-14 16:46:33 +0700

    Update remove.js

--- remove.js
+++ remove.js
@@ -1,6 +1,19 @@
-document.querySelectorAll('h4').forEach(h4 => {
-    if (h4.querySelector('.ver_text_time')) {
-        h4.remove();
+(function(){
+    function waitForElement(selector, callback) {
+        const el = document.querySelector(selector);
+        if (el) {
+            callback(el);
+        } else {
+            setTimeout(() => waitForElement(selector, callback), 300);
+        }
     }
-});
-console.log('Элемент удалён!');
+
+    waitForElement('h4 .ver_text_time', () => {
+        document.querySelectorAll('h4').forEach(h4 => {
+            if (h4.querySelector('.ver_text_time')) {
+                h4.remove();
+            }
+        });
+        console.log('Элемент h4 с датой удалён');
+    });
+})();
